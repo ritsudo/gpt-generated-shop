448c9af49e3104ce9000fc99662980993321f690
ritsudo <minato-namikaje@yandex.ru>
2024-01-03 23:38:22 +0300

    feat: added item service

--- src/app/layout/product-list/product-list.component.ts
+++ src/app/layout/product-list/product-list.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ItemService } from '../../services/item.service';
+import { Item } from '../../Dto/item.model';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
 
+  constructor(
+    private itemService: ItemService
+  ) {}
+
+  items!: Observable<Item[]>;
+
+  ngOnInit(): void {
+    this.items = this.itemService.getItems();
+  }
 }
--- src/app/services/item.service.ts
+++ src/app/services/item.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Item } from '../Dto/item.model';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class ItemService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getItems() {
+    return this.http.get<Item[]>('/assets/items.json');
+  }
 }
